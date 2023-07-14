0ff931699694f563fcd211d7812c1d7a2ff25765
AdityaRajeshJadhav2003 <121690879+AdityaRajeshJadhav2003@users.noreply.github.com>
2023-07-14 10:33:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Software Engineer", "Btech Student", "Programmer", "Problem Solver"],
+    typeSpeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+});
